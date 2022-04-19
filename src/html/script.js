55b5f106ea4aeb49171f561664085c9e327523da
Mark Biesheuvel <mail@markbiesheuvel.nl>
2022-04-19 11:35:41 +0200

    Replace custom JavaScript with forced query parameters, replace custom event with click event

--- src/html/script.js
+++ src/html/script.js
@@ -7,10 +7,10 @@
 
   // Load data from Optimizely
   const optimizely = window.optimizely
-  const data = optimizely.get('data')
 
   // Push a custom attribute indicating whether the visitor is a canary or not
   // Amazon CloudWatch Synthetics Canaries use a unique User Agent
+  // TODO: store this in project JavaScript
   const isCanary = window.navigator.userAgent.includes('CloudWatchSynthetics')
   optimizely.push({
     type: 'user',
@@ -18,32 +18,6 @@
       isCanary: String(isCanary)
     }
   })
-
-  // Enable logging
-  optimizely.push({
-    type: 'log',
-    level: 'INFO'
-  })
-
-  // Function to put a user into a particular experiment
-  const bucketVisitor = (variationIndex) => {
-    for (const experimentId in data.experiments) {
-      optimizely.push({
-        type: 'bucketVisitor',
-        experimentId: experimentId,
-        variationIndex: variationIndex
-      })
-    }
-    window.location.reload()
-  }
-
-  // Function to send addToCart event
-  const addToCart = () => {
-    optimizely.push({
-      type: 'event',
-      eventName: 'addToCart'
-    })
-  }
 
   // Function to send revenue event
   const purchase = () => {
@@ -58,23 +32,7 @@
 
   // Function to select buttons and add listeners to them
   const addButtonListeners = () => {
-    const buttonOriginal = document.querySelector('button#original')
-    const buttonVariation1 = document.querySelector('button#variation1')
-    const buttonAddToCart = document.querySelector('button#addtocart')
-    const buttonPurchase = document.querySelector('button#purchase')
-
-    buttonOriginal.onclick = () => {
-      bucketVisitor(0)
-    }
-    buttonVariation1.onclick = () => {
-      bucketVisitor(1)
-    }
-    buttonAddToCart.onclick = () => {
-      addToCart()
-    }
-    buttonPurchase.onclick = () => {
-      purchase()
-    }
+    document.querySelector('button#purchase').onclick = purchase
   }
 
   // Wait until the DOM is loaded before adding button listeners
